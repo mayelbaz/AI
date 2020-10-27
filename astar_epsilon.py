63efdcdc910eea161f2c0ce0ab8ab7b291f96956
mayelbaz <mayelbaz123@gmail.com>
2020-10-27 16:52:46 +0200

    Update astar_epsilon.py

--- astar_epsilon.py
+++ astar_epsilon.py
@@ -6,11 +6,6 @@ import math
 
 
 class AStarEpsilon(AStar):
-    """
-    This class implements the (weighted) A*Epsilon search algorithm.
-    A*Epsilon algorithm basically works like the A* algorithm, but with
-    another way to choose the next node to expand from the open queue.
-    """
 
     solver_name = 'A*eps'
 
@@ -35,39 +30,7 @@ class AStarEpsilon(AStar):
         super(AStarEpsilon, self)._init_solver(problem)
 
     def _extract_next_search_node_to_expand(self, problem: GraphProblem) -> Optional[SearchNode]:
-        """
-        Extracts the next node to expand from the open queue,
-         by focusing on the current FOCAL and choosing the node
-         with the best within_focal_priority from it.
-        TODO [Ex.38]: Implement this method!
-        Find the minimum expanding-priority value in the `open` queue.
-        Calculate the maximum expanding-priority of the FOCAL, which is
-         the min expanding-priority in open multiplied by (1 + eps) where
-         eps is stored under `self.focal_epsilon`.
-        Create the FOCAL by popping items from the `open` queue and inserting
-         them into a focal list. Don't forget to satisfy the constraint of
-         `self.max_focal_size` if it is set (not None).
-        Notice: You might want to pop items from the `open` priority queue,
-         and then choose an item out of these popped items. Don't forget:
-         the other items have to be pushed back into open.
-        Inspect the base class `BestFirstSearch` to retrieve the type of
-         the field `open`. Then find the definition of this type and find
-         the right methods to use (you might want to peek the head node, to
-         pop/push nodes and to query whether the queue is empty).
-        For each node (candidate) in the created focal, calculate its priority
-         by callingthe function `self.within_focal_priority_function` on it.
-         This function expects to get 3 values: the node, the problem and the
-         solver (self). You can create an array of these priority value. Then,
-         use `np.argmin()` to find the index of the item (within this array)
-         with the minimal value. After having this index you could pop this
-         item from the focal (at this index). This is the node that have to
-         be eventually returned.
-        Don't forget to handle correctly corner-case like when the open queue
-         is empty. In this case a value of `None` has to be returned.
-        Note: All the nodes that were in the open queue at the beginning of this
-         method should be kept in the open queue at the end of this method, except
-         for the extracted (and returned) node.
-        """
+     
         if self.open.is_empty():
             return None
 
